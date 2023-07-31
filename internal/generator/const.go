73f7e45ca43fe6ad72c22bc13ac7f8eb2844d0dd
Diego Rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-07-31 11:33:52 -0300

    [ADD] constants

---
+++ internal/generator/const.go
@@ -0,0 +1,8 @@
+package generator
+
+const (
+	upperChars   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lowerChars   = "abcdefghijklmnopqrstuvwxyz"
+	numberChars  = "0123456789"
+	specialChars = "!@#$%^&*()-_=+,.?/:;{}[]~"
+)
